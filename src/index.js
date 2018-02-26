b219001bb075dd762bdcae3c2d128b670009f91f
shuiqin <xushuiqin@meituan.com>
2018-02-26 17:30:53 +0800

    update

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@
 export default function printMe() {
   console.log('I get called from print.js!');
   this.alert('Hmmm, this probably isn\'t a great idea...');
-}
 
-console.log(
-  _.join(['Another', 'module', 'loaded!'], ' ')
-);
+  console.log(
+    _.join(['Another', 'module', 'loaded!'], ' ')
+  );
+}
